95d731abc16ad9ad77295c33033821bc5b24b9df
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-04-28 02:34:29 +0100

    normal test get count

--- test_get_count.py
+++ test_get_count.py
@@ -7,8 +7,5 @@ from models.state import State
 print("All objects: {}".format(storage.count()))
 print("State objects: {}".format(storage.count(State)))
 
-a = storage.all(State)
-print(a)
-
-first_state_id = list(a.values())[0].id
+first_state_id = list(storage.all(State).values())[0].id
 print("First state: {}".format(storage.get(State, first_state_id)))
