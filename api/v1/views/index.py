5137bf21f0fb0b77b301ed123a102c17ec2eb2a1
achrafkassimi <achraf.kassimi.1995@gmail.com>
2024-04-28 18:09:22 +0100

    fix style task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -22,12 +22,12 @@ def get_stats():
     """
     """
     stats = {
-        "amenities" : storage.count('Amenity'),
-        "cities" : storage.count('City'),
-        "places" : storage.count('Place'),
-        "reviews" : storage.count('Review'),
-        "states" : storage.count('State'),
-        "users" : storage.count('User')
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
     }
     return jsonify(stats)
 
